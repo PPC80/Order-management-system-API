362197f3f83693533532b323fe3c19f014f22bb2
Pedro Páez <pedro_ppc@hotmail.com>
2023-07-19 23:33:06 -0500

    test deploy

--- routes/api.php
+++ routes/api.php
@@ -33,8 +33,8 @@ Route::post('register', [AuthController::class, 'register']);
 Route::get('products',[ProductsController::class,'index']);
 Route::get('products/search',[ProductsController::class,'search']);
 
-Route::middleware(['auth:sanctum'])->group(function ()
-{
+Route::middleware(['auth:sanctum'])->group(function (){
+    
     //Accounts
     Route::get('accounts',[AuthController::class,'indexAccounts']);
 
